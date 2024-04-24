288c4a88538ae6fe7ef5d19b53194b78a2d2bdfa
bronto051 <igor_1990@live.com>
2024-04-24 19:13:43 +0200

    Imlementing core game mechanic (up to video 29)

---
+++ Assets/Scripts/Background Scripts/Collectors/BGCollector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGCollector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Background")
+        {
+            target.gameObject.SetActive(false);
+
+        }
+
+    }
+
+
+}
